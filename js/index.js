5561be356933092680d9b75b8bf6ab30dce65a8a
Plasm3rA0 <p.hernan.camino2005@gmail.com>
2023-01-19 21:30:45 +0100

    Final

---
+++ js/index.js
@@ -0,0 +1,8 @@
+var canvas = document.getElementById("canvas1");
+var ctx = canvas.getContext("2d");
+ctx.fillStyle = "red";
+ctx.fillRect(50, 60, 50, 70);
+ctx.strokeStyle = "black";
+ctx.strokeRect(50, 60, 50, 70);
+ctx.fillStyle = "red";
+ctx.fillRect(73, 30, 2, 200);
